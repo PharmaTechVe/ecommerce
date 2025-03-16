8a79a79b2eac7b9df8364d2065435f544f116fef
Ricardo J <ricardojimenez0999@gmail.com>
2025-03-16 01:21:51 -0400

    fix colors and delete searchbar

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
+import { Colors } from '../styles/styles';
 type Slide = {
   id: number;
   imageUrl: string;
@@ -11,22 +12,26 @@ type Props = {
 };
 export default function ProductCarousel({ slides }: Props) {
   const [current, setCurrent] = useState(0);
-
   const prevSlide = () =>
     setCurrent((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
   const nextSlide = () =>
     setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
-
   return (
-    <div className="relative mx-auto h-[316px] w-[1200px] max-w-6xl overflow-visible rounded-xl bg-[#1C1E39]">
+    <div
+      className="relative mx-auto h-[316px] w-[1200px] max-w-6xl overflow-visible rounded-xl"
+      style={{ backgroundColor: Colors.primary }}
+    >
       <button
         onClick={prevSlide}
-        className="absolute left-[-35px] top-1/2 z-10 flex -translate-y-1/2 items-center justify-center rounded-full border-[10px] border-white bg-[#A3E4D7]"
-        style={{ width: '86px', height: '86px' }}
+        className="absolute left-[-35px] top-1/2 z-10 flex -translate-y-1/2 items-center justify-center rounded-full border-[10px] border-white"
+        style={{
+          width: '86px',
+          height: '86px',
+          backgroundColor: Colors.semanticInfo,
+        }}
       >
         <ChevronLeftIcon className="h-14 w-7 text-white" />
       </button>
-
       <div className="relative h-full w-full overflow-hidden rounded-xl">
         {slides.map((slide, index) => (
           <div
@@ -46,8 +51,12 @@ export default function ProductCarousel({ slides }: Props) {
       </div>
       <button
         onClick={nextSlide}
-        className="absolute right-[-35px] top-1/2 z-10 flex -translate-y-1/2 items-center justify-center rounded-full border-[10px] border-white bg-[#A3E4D7]"
-        style={{ width: '86px', height: '86px' }}
+        className="absolute right-[-35px] top-1/2 z-10 flex -translate-y-1/2 items-center justify-center rounded-full border-[10px] border-white"
+        style={{
+          width: '86px',
+          height: '86px',
+          backgroundColor: Colors.semanticInfo,
+        }}
       >
         <ChevronRightIcon className="h-14 w-7 text-white" />
       </button>
