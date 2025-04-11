285f32ae9cd90b83f6c268389689dc681e0b26ab
ClarissaRun <yohannryz@gmail.com>
2025-04-10 23:48:21 -0400

    minor change

--- src/lib/validations/updatePasswordSchema.ts
+++ src/lib/validations/updatePasswordSchema.ts
@@ -9,11 +9,11 @@ export const updatePasswordSchema = z
     newPassword: z
       .string()
       .nonempty('La contraseña es obligatoria')
-      .min(6, 'La contraseña debe tener al menos 8 caracteres'),
+      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
     confirmPassword: z
       .string()
       .nonempty('La confirmación es obligatoria')
-      .min(6, 'La contraseña debe tener al menos 8 caracteres'),
+      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: 'Las contraseñas no coinciden',
