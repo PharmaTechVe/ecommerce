ff8481c6ba8888e9e2bec5b39ebb60471dd8581f
Gustavo <1001.29587789.ucla@gmail.com>
2025-05-18 00:18:56 -0400

    updatePasswordSchema: add newPassword rules

--- src/lib/validations/updatePasswordSchema.ts
+++ src/lib/validations/updatePasswordSchema.ts
@@ -2,18 +2,21 @@ import { z } from 'zod';
 
 export const updatePasswordSchema = z
   .object({
-    password: z
-      .string()
-      .nonempty('La contraseña es obligatoria')
-      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
+    password: z.string().nonempty('La contraseña actual es obligatoria'),
+
     newPassword: z
       .string()
       .nonempty('La contraseña es obligatoria')
-      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
-    confirmPassword: z
-      .string()
-      .nonempty('La confirmación es obligatoria')
-      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
+      .min(8, 'La contraseña debe tener al menos 8 caracteres')
+      .regex(/[A-Z]/, 'Debe tener al menos una letra mayúscula')
+      .regex(/[a-z]/, 'Debe tener al menos una letra minúscula')
+      .regex(/\d/, 'Debe tener al menos un número')
+      .regex(
+        /[!@#$%^&*]/,
+        'Debe tener al menos un símbolo especial (!@#$%^&*)',
+      ),
+
+    confirmPassword: z.string().nonempty('La confirmación es obligatoria'),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: 'Las contraseñas no coinciden',
