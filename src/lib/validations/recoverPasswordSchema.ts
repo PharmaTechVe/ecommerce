3eb8996d31694c042f5e61746821b5575db16b36
Gustavo <1001.29587789.ucla@gmail.com>
2025-05-18 00:33:05 -0400

    recoverPasswordSchema: add newPassword rules

--- src/lib/validations/recoverPasswordSchema.ts
+++ src/lib/validations/recoverPasswordSchema.ts
@@ -10,11 +10,16 @@ export const resetPasswordSchema = z
     newPassword: z
       .string()
       .nonempty('La contraseña es obligatoria')
-      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
-    confirmPassword: z
-      .string()
-      .nonempty('La confirmación es obligatoria')
-      .min(8, 'La contraseña debe tener al menos 8 caracteres'),
+      .min(8, 'La contraseña debe tener al menos 8 caracteres')
+      .regex(/[A-Z]/, 'Debe tener al menos una letra mayúscula')
+      .regex(/[a-z]/, 'Debe tener al menos una letra minúscula')
+      .regex(/\d/, 'Debe tener al menos un número')
+      .regex(
+        /[!@#$%^&*]/,
+        'Debe tener al menos un símbolo especial (!@#$%^&*)',
+      ),
+
+    confirmPassword: z.string().nonempty('La confirmación es obligatoria'),
   })
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: 'Las contraseñas no coinciden',
